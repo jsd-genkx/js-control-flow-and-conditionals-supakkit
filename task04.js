de3e7b830e7390d3cbf71bd07542b996f6dbbc0f
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-15 12:20:51 +0700

    add(task04.js): added my code into task04.js

--- task04.js
+++ task04.js
@@ -6,6 +6,35 @@
 // Divisible by only 5 → "Buzz"
 // Otherwise → "Not divisible by 3 or 5"
 
+
+
+// const num = 15;
+const num = 9;
+
+if ( num % 3 === 0 && num % 5 === 0 ) {
+    console.log( "FizzBuzz" );
+}
+else if ( num % 3 === 0 ) {
+    console.log( "Fizz" );
+}
+else if ( num % 5 === 0 ) {
+    console.log( "Buzz" );
+}
+else {
+    console.log( "Not divisible by 3 or 5" )
+}
+    
 // TODO BONUS: Rewrite with if
 
-const num = 15;
+// if ( num % 3 === 0 && num % 5 === 0 ) {
+//     console.log( "FizzBuzz" );
+// }
+// if ( num % 3 === 0 && num % 5 !== 0 ) {
+//     console.log( "Fizz" );
+// }
+// if ( num % 3 !== 0 && num % 5 === 0 ) {
+//     console.log( "Buzz" );
+// }
+// if ( num % 3 !== 0 && num % 5 !== 0 ) {
+//     console.log( "Not divisible by 3 or 5" );
+// }

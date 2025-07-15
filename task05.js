66775ea0dd44fa3d9829f2566c1663f96793ca7f
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-15 12:27:17 +0700

    add(task05.js): added my code into task05.js

--- task05.js
+++ task05.js
@@ -2,14 +2,29 @@
 
 // TODO: Convert this if to switch/case (You can add more options.)
 
-const fruit = "banana";
+// const fruit = "banana";
+const fruit = "pear";
 
-if (fruit === "apple") {
-	console.log("An apple a day keeps the doctor away.");
-} else if (fruit === "banana") {
-	console.log("Banana is good for potassium.");
-} else if (fruit === "orange") {
-	console.log("Oranges are full of vitamin C.");
-} else {
-	console.log("Unknown fruit.");
+// if (fruit === "apple") {
+// 	console.log("An apple a day keeps the doctor away.");
+// } else if (fruit === "banana") {
+// 	console.log("Banana is good for potassium.");
+// } else if (fruit === "orange") {
+// 	console.log("Oranges are full of vitamin C.");
+// } else {
+// 	console.log("Unknown fruit.");
+// }
+
+switch(fruit) {
+	case "apple":
+		console.log("An apple a day keeps the doctor away.");
+		break;
+	case "banana":
+		console.log("Banana is good for potassium.");
+		break;
+	case "orange":
+		console.log("Oranges are full of vitamin C.");
+		break;
+	default:
+		console.log("Unknown fruit.");
 }

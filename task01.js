30ebb79dd1e5309fa407ce3327e1d4da6fd47633
supakkit <87434100+supakkit@users.noreply.github.com>
2025-07-15 12:18:40 +0700

    add(task01.js&task02.js): added my code into task01.js and task02.js

--- task01.js
+++ task01.js
@@ -3,3 +3,7 @@
 // TODO: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+
+if( number % 2 === 0) console.log("Even");
+else console.log("Odd");
+
--- task02.js
+++ task02.js
@@ -3,5 +3,11 @@
 // TODO: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+// const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+
+if ( typeof userInput === 'number' && !isNaN(userInput) )
+    console.log( "it's a number" );
+else
+    console.log( "it's not a number" );
